b5a7a8ac93e35fce9d51aa15c482c2b426ff4884
cienpi272003 <cienpi272003@gmail.com>
2026-05-13 22:26:45 +0700

    new

---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+import { HeaderOnly } from '~/component/Layout';
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Upload from '~/pages/Upload';
+import Profile from '~/pages/Profile';
+import Search from '~/pages/Search';
+
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/profile', component: Profile, layout: null },
+    { path: '/search', component: Search, layout: null },
+];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
